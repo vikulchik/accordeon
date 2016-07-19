9c4c2b8921d7448867a8e6a67a5670a8163fcf4c
vik <kvvendetta@gmail.com>
2016-07-19 16:33:39 +0300

    Change init.js

--- dev/js/modules/init.js
+++ dev/js/modules/init.js
@@ -1,11 +1,14 @@
 ;(function() {
     var link = document.getElementsByClassName('main_link');
-    for (var i = 0; i < link.length; i++) {
+    var list = document.getElementsByClassName('main_list');
+    /*for (var i = 0; i < link.length; i++) {
         link[i].addEventListener("click", function (e) {
             e.preventDefault();
             this.nextElementSibling.classList.toggle('active');
         });
-   }
-
-
+   }*/
+    list.addEventListener('click', function (e) {
+        console.log('кликнули на' + e.target.getAttribute('my-attr'));
+        link.nextElementSibling.classList.toggle('active');
+    })
 })();
